5bca7a0bffebf8c21e16a9430e2e572939abd904
chawitkhonyong23 <s42880@bj.ac.th>
2025-01-03 15:00:36 +0700

    Add files via upload

---
+++ Midterm-23-23/offline.js
@@ -0,0 +1,14 @@
+﻿{
+	"version": 1735889504,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/player-sheet0.png",
+		"images/coin-sheet0.png",
+		"images/coinbox-sheet0.png",
+		"images/ground.png"
+	]
+}
